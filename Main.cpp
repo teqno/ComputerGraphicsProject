012cec52c4d6b4f1b13324f8ec27584e950a4b42
teqno <teqno8@gmail.com>
2024-02-06 15:43:35 +0200

    Add perspective and 3d pyramid

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Texture.h"
 #include "shaderClass.h"
@@ -9,20 +12,28 @@
 #include "VBO.h"
 #include "EBO.h"
 
+const unsigned int width = 800;
+const unsigned int height = 800;
+
 // Vertices coordinates
 GLfloat vertices[] =
-{ //	COORDINATES		  /			COLORS		  /   TexCoord	//
-	-0.5f,	-0.5f,	0.0f,	1.0f,	0.0f,	0.0f,	0.0f, 0.0f,	// Lower left
-	-0.5f,	 0.5f,	0.0f,	0.0f,	1.0f,	0.0f,	0.0f, 1.0f,	// Lower right
-	 0.5f,	 0.5f,	0.0f,	0.0f,	0.0f,	1.0f,	1.0f, 1.0f,	// Top middle
-	 0.5f,	-0.5f,	0.0f,	1.0f,	1.0f,	1.0f,	1.0f, 0.0f,	// Inner left
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
-// Indices that indicate the order of walking through the vertices
+// Indices for vertices order
 GLuint indices[] =
 {
-	0, 2, 1,	// Upper triangle
-	0, 3, 2		// Lower triangle
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
 int main() {
@@ -36,7 +47,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create GLFWwindow object of width and hight 800 and 800 pixels
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Computer Graphics Project", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Computer Graphics Project", NULL, NULL);
 	// If the window doesn't exist then exit the program
 	if (window == NULL) {
 		std::cout << "Failed to create window" << std::endl;
@@ -50,7 +61,7 @@ int main() {
 	gladLoadGL();
 
 	// Specify the viewport of OpenGL in the window 
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	// Creates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
@@ -74,23 +85,59 @@ int main() {
 	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	GLuint modelUniID = glGetUniformLocation(shaderProgram.ID, "model");
+	GLuint viewUniID = glGetUniformLocation(shaderProgram.ID, "view");
+	GLuint projUniID = glGetUniformLocation(shaderProgram.ID, "proj");
 	
 	// Texture
-	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture popCat("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);
+
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	// Enables depth buffer
+	glEnable(GL_DEPTH_TEST);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window)) {
+		// Color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		// Clean the back buffer and depth buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Tell OpenGL which shader program we want to use
 		shaderProgram.Activate();
+
+		// Simple timer
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1 / 60) {
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+
+		// Initialize matrices so they aren't null
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		// Transform matrices
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), float(width / height), 0.1f, 100.0f);
+
+		// Pass matrices to vertex shader as uniforms
+		glUniformMatrix4fv(modelUniID, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewUniID, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projUniID, 1, GL_FALSE, glm::value_ptr(proj));
+
+		// Assign a value to the uniform. This must be done after activating the shader program.
 		glUniform1f(uniID, 0.5f);
 		popCat.Bind();
 		// Bind VAO for OpenGL to use it
 		VAO1.Bind();
 		// Draw the triangles using the GL_TRIANGLES primitive
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Process GLFW events
